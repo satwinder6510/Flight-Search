2650fd60b53ccbae4fffaa644c22c526042d2551
satwinder6510 <142193859+satwinder6510@users.noreply.github.com>
2025-04-14 14:23:02 +0100

    pages

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/Flight-Search/'
   },
   module: {
     rules: [
